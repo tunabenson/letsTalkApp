beef60335003ce311c5742077f52b977d53d7d3e
tunabenson <121179065+tunabenson@users.noreply.github.com>
2024-07-12 22:27:00 -0700

    Update firebaseConfig.js

--- LetsTalk/api/firebaseConfig.js
+++ LetsTalk/api/firebaseConfig.js
@@ -10,13 +10,6 @@ import algoliasearch from "algoliasearch";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClM0A71FzMh5hnxVoa0Meu2FUP2rF14BY",
-  authDomain: "letstalk-e7a23.firebaseapp.com",
-  projectId: "letstalk-e7a23",
-  storageBucket: "letstalk-e7a23.appspot.com",
-  messagingSenderId: "778362832452",
-  appId: "1:778362832452:web:5e268785fd76e242ff1e11",
-  measurementId: "G-3QB3TRV9H0"
 };
 
 // Initialize Firebase
@@ -27,7 +20,7 @@ export const auth=initializeAuth(app, {persistence:getReactNativePersistence(Asy
 
 export const storage= getStorage(app);
 
-export const searchClient= algoliasearch('GQZGLJWQVJ','305db113298fb6ea6a615192fbf59ced');
+export const searchClient= algoliasearch();
 
 export const db= initializeFirestore(app, {cacheSizeBytes: CACHE_SIZE_UNLIMITED});
 
